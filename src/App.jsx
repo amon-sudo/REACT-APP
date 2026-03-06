21acc27b56222f2a3314acbc6c34feef8bd35e84
amon-sudo <amonogino@gmail.com>
2026-03-06 23:49:21 +0300

    starting n

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
-
-function App() {
+import Layout from "./components/layouts/layout"
+import Welcome from "./components/layouts/Welcome"
+import Dashboard from "./components/layouts/Dashboard"
+import Challenge from "./components/Challenge"
+function App(props) {
 
 
   return (
     <>
-        <Layout
+    
+        
+        
+        
+        <Layout>
+
+                <Welcome/>
+                <Dashboard />
+                <Challenge />
+        </Layout>
+ 
+        
+        
+        
     </>
   )
 }
--- src/components/layouts/layout.jsx
+++ src/components/layouts/layout.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { Children } from 'react'
 
-function layout() {
+function Layout(props) {
+
+  const {children} = props
   return (
     <>
-            <div>layout</div>l
+    
+          <header>
+            <h1>EL GRINGO LEARN</h1>
+          </header>
+          {children}
+          <footer>
+            <small>Created by</small>
+            <a href='#' target='_blank'>
+
+            </a>
+            ICON
+          </footer>
     
     </>
   )
 }
 
-export default layout
+export default Layout
