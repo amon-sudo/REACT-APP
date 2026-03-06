f262ec53ad2102fb86aa5c9212295f2e7a27f04b
amon-sudo <amonogino@gmail.com>
2026-03-07 00:32:58 +0300

    starting

--- src/components/layouts/Welcome.jsx
+++ src/components/layouts/Welcome.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 function Welcome() {
   return (
     <>
-        <div></div>
+        <section id='welcome'>
+            <h3 className='text-large special-shadow'>
+                365 days.<br/> 365 words.
+            </h3>
+            <h6>Build your lexicon. <br/> Start your challenge today</h6>
+            <div>
+                <input type='text' placeholder='enter your name...'/>
+                <button>
+                    <h6>Start &rr;</h6>
+                </button>
+            </div>
+        </section>
     </>
     
   )
--- src/components/layouts/layout.jsx
+++ src/components/layouts/layout.jsx
@@ -7,15 +7,16 @@ function Layout(props) {
     <>
     
           <header>
-            <h1>EL GRINGO LEARN</h1>
+            <h1 className='text-gradient'>EL GRINGO LEARN</h1>
           </header>
           {children}
           <footer>
             <small>Created by</small>
-            <a href='#' target='_blank'>
-
+            <a href='https://github.com/amon-sudo' target='_blank'>
+            <img href='' alt='pfp'></img>
+            <p>@EL gringo</p>
             </a>
-            ICON
+             <i className="fa-brands fa-github" ></i>
           </footer>
     
     </>
