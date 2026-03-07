a1c5627a014f80688ae7a69436cfa7f9c2445031
amon-sudo <amonogino@gmail.com>
2026-03-07 23:02:04 +0300

    ivve now dealt with the word bars

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
 function Challenge() {
+    const word = "copacitic"
+    const definiton = "in excellent order"
   return (
     <>
     <section id='challlenge'>
-        
+        <h1>{word}</h1>
+        <p>in excellen order</p>
+        <div className='helper'>
+            <div>
+                {/* the error correction bars come into this div */}
+                {[...Array(definiton.length).keys()]
+                .map((element, elementIdx) =>{
+                    return(
+                        <div key={elementIdx}>aa</div>
+                    )
+                })}
+            </div>
+        </div>
     </section>
     </>
   )
