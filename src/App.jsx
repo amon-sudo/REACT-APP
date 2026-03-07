2b186700321394b520440a6e30ef28650d72283b
amon-sudo <amonogino@gmail.com>
2026-03-07 19:54:47 +0300

    adde the json and the index file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Welcome from "./components/layouts/Welcome"
 import Dashboard from "./components/layouts/Dashboard"
 import Challenge from "./components/Challenge"
 function App(props) {
-  const selectedPage = 1  // 0 is for the welcome page, 1 is for the dashbaord and 2 is for the challenge page
+  const selectedPage = 2  // 0 is for the welcome page, 1 is for the dashbaord and 2 is for the challenge page
   const pages = {
     0: <Welcome />,
     1: <Dashboard />,
